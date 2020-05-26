8015dc08c0ec67a8646ace3e1efea50664d49453
WAY29 <1162410187@qq.com>
2020-05-26 08:01:14 +0800

    fix a bug

--- general/help.py
+++ general/help.py
@@ -9,45 +9,50 @@ def run(order: str = "help"):
 
     Output the help document for the command. Defaults to 'help'.
 
-    Args:
-        order (str): The order name
+    eg: help {order=help}
     """
-    pf = gget(f"{gget('namespace')}.pf")
+    pf = None
+    gpf = gget(f"general.pf")
+    npf = gget(f"{gget('namespace')}.pf")
     order = order_alias(order)
-    api = gget("api")
-    if order in pf:
-        func = getattr(pf[order], api)
-        if (func.__doc__):
-            print(func.__doc__)
-        block = " " * 4
-        block_two = block * 2
-        sig = inspect.signature(func)
-        folders_namespace = gget("folders_namespace")
-        func_folder, func_name = func.__module__.split(".")
-        func_reverse_alias = gget(
-            "%s.reverse_alias" % func_name, folders_namespace[func_folder]
-        )
-        if (len(sig.parameters)):
-            print("%sCommand Args:" % block)
-        for k, v in sig.parameters.items():
-            arg = "--%s" % k
-            if k in func_reverse_alias:
-                arg = "-%s,%s" % (func_reverse_alias[k], arg)
-            desc = str(v).split(":")
-            if len(desc) > 1:  # 存在参数类型
-                desc = desc[1].split("=")
-                if len(desc) > 1:  # 存在默认值
-                    desc = "[%s] %s (Default: %s)" % (
-                        desc[0].strip(),
-                        k,
-                        desc[1].strip(),
-                    )
-                else:
-                    desc = "[%s] %s" % (desc[0].strip(), k)
-                    arg = "%s(*)" % arg
-            else:
-                desc = "[?] %s" % desc[0]
-                arg = "%s(*)" % arg
-            print("%s%-20s%s%s\n" % (block_two, arg, block_two, desc))
+    if (order in gpf):
+        pf = gpf
+    elif (order in npf):
+        pf = npf
     elif order:
         print("%s object is not command-function" % order)
+        return
+    api = gget("api")
+    func = getattr(pf[order], api)
+    if (func.__doc__):
+        print(func.__doc__)
+    block = " " * 4
+    block_two = block * 2
+    sig = inspect.signature(func)
+    folders_namespace = gget("folders_namespace")
+    func_folder, func_name = func.__module__.split(".")
+    func_reverse_alias = gget(
+        "%s.reverse_alias" % func_name, folders_namespace[func_folder]
+    )
+    if (len(sig.parameters)):
+        print("%sCommand Args:" % block)
+    for k, v in sig.parameters.items():
+        arg = "--%s" % k
+        if k in func_reverse_alias:
+            arg = "-%s,%s" % (func_reverse_alias[k], arg)
+        desc = str(v).split(":")
+        if len(desc) > 1:  # 存在参数类型
+            desc = desc[1].split("=")
+            if len(desc) > 1:  # 存在默认值
+                desc = "[%s] %s (Default: %s)" % (
+                    desc[0].strip(),
+                    k,
+                    desc[1].strip(),
+                )
+            else:
+                desc = "[%s] %s" % (desc[0].strip(), k)
+                arg = "%s(*)" % arg
+        else:
+            desc = "[?] %s" % desc[0]
+            arg = "%s(*)" % arg
+        print("%s%-20s%s%s\n" % (block_two, arg, block_two, desc))
