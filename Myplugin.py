410b17c8255d08ad23a77f19c73c647c7d4b782c
WAY29 <1162410187@qq.com>
2020-06-20 16:46:46 +0800

    Add db_dump command

--- Myplugin.py
+++ Myplugin.py
@@ -52,7 +52,7 @@ class Platform():
                         '.' + pluginName + ' Failed Loaded by missing required.'
 
         except ImportError as e:  # 导入错误
-            print(e)
+            # print(e)
             if (self.__message is True):
                 self.__message_dict[pluginName] = self.plugin_path + \
                     '.' + pluginName + ' Failed Loaded. ' + str(e)
