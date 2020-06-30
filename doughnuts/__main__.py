34a17d050b8816ba9b10238cef46c0bb26c188a1
WAY29 <1162410187@qq.com>
2020-06-30 22:36:30 +0800

    update __main__

--- doughnuts/__main__.py
+++ doughnuts/__main__.py
@@ -1,2 +1,6 @@
-from doughnuts import main
+from .doughnuts import main
+from os import path
+from sys import path as fpath
+cpath = path.split(path.realpath(__file__))[0]
+fpath.append(cpath)
 main()
