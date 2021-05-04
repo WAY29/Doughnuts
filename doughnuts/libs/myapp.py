6d4e51d92fb1ab0aa7bfa096cbd1b48f682981a8
WAY29 <1162410187@qq.com>
2021-05-04 20:18:59 +0800

    Bump version: 4.10.5 → 4.11.0

--- doughnuts/libs/myapp.py
+++ doughnuts/libs/myapp.py
@@ -33,7 +33,7 @@ ALPATHNUMERIC = ascii_letters + digits
 RAND_KEY = str(uuid4())
 UNITS = {"B": 1, "KB": 2**10, "MB": 2**20, "GB": 2**30, "TB": 2**40}
 
-__version__ = "4.10.5"
+__version__ = "4.11.0"
 
 
 disable_warnings()
